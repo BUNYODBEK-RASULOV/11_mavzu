d4fca8b458d2f6127577310caf4f02aaa8be99b6
BUNYODBEK RASULOV <87300708+BUNYODBEK-RASULOV@users.noreply.github.com>
2021-08-02 09:32:16 +0500

    11-mavzu misollari

---
+++ Qudrat_Abduraximovni_savollari_11mavzu.py
@@ -0,0 +1,120 @@
+# a=(float)(input("Sonni kiriting"))
+# if (a > 0):
+#     print("a=",a+1)
+# else:
+#     print("a=",a)
+
+#2-topshiriq
+# a = (float)(input("Sonni kiriting:"))
+# if (a > 0):
+#     print("a=", a + 1)
+# else:
+#     print("a=", a-2)
+
+#3-topshiriq
+# a = (float)(input("Sonni kiriting:"))
+# if (a > 0):
+#     print("a=", a + 1)
+# elif(a==0):
+#     print("a=",10)
+# else:
+#     print("a=", a-2)
+
+
+#4-topshiriq
+# a = (float)(input("a Sonni kiriting:"))
+# b = (float)(input("b Sonni kiriting:"))
+# c = (float)(input("c Sonni kiriting:"))
+# i=0
+# a=(a,b,c,)
+# while (i<3):
+#     if(a[i]>0):
+#         print(a[i])
+#     i = i + 1
+
+#5-topshiriq
+#
+
+#6- topshiriq
+# a = (float)(input("a Sonni kiriting:"))
+# b = (float)(input("b Sonni kiriting:"))
+# if a > b:
+#     print(a, ">", b)
+# elif a == b:
+#     print("ikkisi teng")
+# else:
+#     print(a, "<", b)
+
+
+
+# 17-topshiriq
+# a = (float)(input("a Sonni kiriting:"))
+# b = (float)(input("b Sonni kiriting:"))
+# c = (float)(input("c Sonni kiriting:"))
+# if ((a > b and b>c) or (a<b and b<c)):
+#     print(2*a)
+#     print(2*b)
+#     print(2*c)
+# else:
+#     print(-a)
+#     print(-b)
+#     print(-c)
+
+
+#12-topshiriq
+# a = (float)(input("a Sonni kiriting:"))
+# b = (float)(input("b Sonni kiriting:"))
+# c = (float)(input("c Sonni kiriting:"))
+# x = a
+# if b < x:
+#     x = b
+# if c < x:
+#     x = c
+# print(x)
+
+
+#18-topshiriq
+# a = (float)(input("a Sonni kiriting:"))
+# b = (float)(input("b Sonni kiriting:"))
+# c = (float)(input("c Sonni kiriting:"))
+# if a==b:
+#     x = b
+#     print(3)
+# elif a==c:
+#     print(2)
+# else:
+#     print(1)
+
+
+#19topshiriq
+# a = (float)(input("a Sonni kiriting:"))
+# b = (float)(input("b Sonni kiriting:"))
+# c = (float)(input("c Sonni kiriting:"))
+# d = (float)(input("d Sonni kiriting:"))
+# if a==b==c:
+#     print(4)
+# elif a==b==d:
+#     print(3)
+# elif b==c==d:
+#     print(1)
+# elif a==c==d:
+#     print(2)
+
+
+
+
+#20-topshiriq
+# a = (float)(input("a Sonni kiriting:"))
+# b = (float)(input("b Sonni kiriting:"))
+# c = (float)(input("c Sonni kiriting:"))
+# if abs(a-b)>abs(a-c):
+#     print(abs(a-c))
+#     print("kordinatasi:",c)
+# elif abs(a-b)<abs(a-c):
+#     print(abs(a-b))
+#     print("kordinatasi",b)
+
+
+
+
+
